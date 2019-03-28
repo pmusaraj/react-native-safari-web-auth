7fa65d38ec63b5f17fdcb2cbdf649ff5e203300a
Penar Musaraj <pmusaraj@gmail.com>
2019-03-28 18:09:55 -0400

    Add Swift Bridging Header

--- ios/SafariWebAuth.swift
+++ ios/SafariWebAuth.swift
@@ -1,3 +1,8 @@
+//
+//  SafariWebAuth.swift
+//  SafariWebAuth
+//
+//
 
 import Foundation
 import AuthenticationServices
@@ -5,20 +10,20 @@ import AuthenticationServices
 @objc(SafariWebAuth)
 @available(iOS 12.0, *)
 class SafariWebAuth: NSObject {
-  var webAuth: ASWebAuthenticationSession?
-
-  @objc func requestAuth(_ authUrl: String) -> Void {
-
-    let oauthUrl = URL(string: authUrl)
-
-    self.webAuth = ASWebAuthenticationSession.init(url: oauthUrl!, callbackURLScheme: "", completionHandler: { (callBack:URL?, error:Error?) in
-      guard error == nil, let successURL = callBack else {print ("canceled"); return}
-      
-      if UIApplication.shared.canOpenURL(successURL) {
-        UIApplication.shared.open(successURL, options: [:], completionHandler: nil)
-      }
-    })
-    self.webAuth?.start()
-
-  }
+    var webAuth: ASWebAuthenticationSession?
+    
+    @objc func requestAuth(_ authUrl: String) -> Void {
+        
+        let oauthUrl = URL(string: authUrl)
+        
+        self.webAuth = ASWebAuthenticationSession.init(url: oauthUrl!, callbackURLScheme: "", completionHandler: { (callBack:URL?, error:Error?) in
+            guard error == nil, let successURL = callBack else {print ("canceled"); return}
+            
+            if UIApplication.shared.canOpenURL(successURL) {
+                UIApplication.shared.open(successURL, options: [:], completionHandler: nil)
+            }
+        })
+        self.webAuth?.start()
+        
+    }
 }
